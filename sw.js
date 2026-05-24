3db4f60b3d39e498d4d8a198c5e6e4c72399cc68
vanessarobayo40-beep <vanessarobayo40@gmail.com>
2026-05-24 15:22:50 -0500

    Agregar service worker PWA

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE='ventas-v1';
+const ASSETS=['./', './index.html',
+  'https://cdn.jsdelivr.net/npm/chart.js@4.4.0/dist/chart.umd.min.js'];
+self.addEventListener('install',e=>{
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+});
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(ks=>
+    Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+self.addEventListener('fetch',e=>{
+  if(!e.request.url.startsWith('http'))return;
+  e.respondWith(caches.match(e.request).then(cached=>{
+    if(cached)return cached;
+    return fetch(e.request).then(res=>{
+      if(res&&res.ok)caches.open(CACHE).then(c=>c.put(e.request,res.clone()));
+      return res;
+    }).catch(()=>cached);
+  }));
+});
